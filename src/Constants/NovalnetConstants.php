3372abb82cadc8fecd9a23d14a8b1e15547be2d2
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-04-27 14:37:19 +0530

    Update NovalnetConstants.php

--- src/Constants/NovalnetConstants.php
+++ src/Constants/NovalnetConstants.php
@@ -31,6 +31,7 @@ class NovalnetConstants
     const SOFORT_PAYMENT_URL = 'https://payport.novalnet.de/online_transfer_payport';
     const BARZAHLEN_LIVEURL = 'https://cdn.barzahlen.de/js/v2/checkout.js';
     const BARZAHLEN_TESTURL = 'https://cdn.barzahlen.de/js/v2/checkout-sandbox.js';
+    const PAYGATE_URI    = 'https://paygate.novalnet.de/paygate.jsp';
     const POSTFINANCE_PAYMENT_URL = 'https://payport.novalnet.de/postfinance';
     const BANCONTACT_PAYMENT_URL = 'https://payport.novalnet.de/bancontact';
 }

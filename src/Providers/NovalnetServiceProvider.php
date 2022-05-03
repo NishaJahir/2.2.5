2729e2016439404fb7ef9e75ecd0f3d0437683b1
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-05-04 01:49:25 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -253,7 +253,7 @@ class NovalnetServiceProvider extends ServiceProvider
             'Novalnet',
             ProcedureEntry::EVENT_TYPE_ORDER,
             $awaitingApprovalFilterTitle,
-            '\Novalnet\Procedures\PaymentStatusFilter@pending'
+            '\Novalnet\Procedures\PaymentStatusFilter@awaitingApproval'
         );
         
         $confirmedFilterTitle = [
